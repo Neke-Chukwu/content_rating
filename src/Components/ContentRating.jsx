10269d35ce58eea97f1491449b3bb29673d9a9a8
Muoebonam Chukwuneke <nekeagust@gmail.com>
2025-01-25 20:17:40 +0100

    Add content rating feature with like and dislike buttons

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,11 +5,29 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+      likes: 0,
+      dislikes: 0
+    };
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+     <div className='content-rating'>
+     <div className='content-rating'>
+        <p>
+        Neke is the smartest person you know. he has a good memory and quick thinking skills.
+        </p>
+        <div className='rating-buttons'>
+        <button className="like-button">
+            Like ({this.state.likes})
+          </button>
+          <button className="dislike-button">
+            Dislike ({this.state.dislikes})
+          </button>
+        </div>
+    </div>
+     </div>
      </>
     );
   }
